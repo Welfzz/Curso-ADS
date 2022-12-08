78a25b53c02dc329a6a22f23c61e5bb83b905e35
Liniker Vaz de Lima <61416550+Welfzz@users.noreply.github.com>
2022-12-08 14:16:31 -0300

    Add files via upload

---
+++ WebApplication3/WebApplication3/clsDvdsModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication3
+{
+    public class clsDvdsModel
+    {
+
+        private SqlConnection oConn;
+        private void openConn()
+        {
+            String strConn;
+            strConn = "Data Source=.\\SQLEXPRESS; initial catalog = DbSisDist; user id = userSisDist; Password = 'user@01'";
+        try
+            {
+                oConn = new SqlConnection(strConn);
+                oConn.Open();
+            }
+            catch { }
+        }
+        private void closeConn()
+        {
+            oConn.Close();
+            oConn = null;
+        }
+        public DataSet getDvdsDataSet(String strSQL, String strName)
+        {
+            SqlDataAdapter da;
+            DataSet ds;
+            try
+            {
+                openConn();
+                da = new SqlDataAdapter(strSQL, oConn);
+                ds = new DataSet();
+                da.Fill(ds, strName);
+                return ds;
+            }
+            finally
+            {
+                closeConn();
+            }
+        }
+
+    }
+}
